c7d318dade2af44037c4f02ffa02590c91692aab
Akshay <akshayphalphale2@gmail.com>
2026-02-10 22:58:48 +0530

    update

--- udemy_training/src/App.jsx
+++ udemy_training/src/App.jsx
@@ -12,9 +12,8 @@ function Header() {
 }
 
 function App() {
-  const [count, setCount] = useState(0);
-  function handleSelect() {
-    console.log("Hello There - slected");
+  function handleSelect(selectedButton) {
+    console.log(selectedButton);
   }
   return (
     <>
@@ -41,10 +40,12 @@ function App() {
       <section id="examples">
         <h2>Examples</h2>
         <menu>
-          <TabButton onSelect={handleSelect}>Components</TabButton>
-          <TabButton onSelect={handleSelect}>JSX</TabButton>
-          <TabButton onSelect={handleSelect}>Props</TabButton>
-          <TabButton onSelect={handleSelect}>State</TabButton>
+          <TabButton onSelect={() => handleSelect("components")}>
+            Components
+          </TabButton>
+          <TabButton onSelect={() => handleSelect("jsx")}>JSX</TabButton>
+          <TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
+          <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
         </menu>
       </section>
     </>
