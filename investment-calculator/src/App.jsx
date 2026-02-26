a2dfb9399e8e0da11de7a5b645e95f6f5cbb795b
Akshay <akshayphalphale2@gmail.com>
2026-02-26 23:35:56 +0530

    Investment calculator app

---
+++ investment-calculator/src/App.jsx
@@ -0,0 +1,36 @@
+import Header from "./components/Header";
+import Results from "./components/Results";
+import UserInput from "./components/UserInput";
+import { useState } from "react";
+
+function App() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
+  function handleChange(inputIdentifier, newValue) {
+    setUserInput((prevUserInput) => {
+      return {
+        ...prevUserInput,
+        [inputIdentifier]: Number(newValue),
+      };
+    });
+  }
+  const inputIsValid = userInput.duration >= 1;
+  return (
+    <>
+      <Header />
+      <UserInput userInput={userInput} onChange={handleChange} />
+      {!inputIsValid && (
+        <p className="error center">
+          Please enter a valid duration (greater than 0 years)
+        </p>
+      )}
+      {inputIsValid && <Results input={userInput} />}
+    </>
+  );
+}
+
+export default App;
